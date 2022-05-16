bda652578031190f41f07e9fea20377c20007352
Dommel Iona <iona.dommel-prioux@epitech.eu>
2022-05-16 15:04:45 +0200

    WIP: express

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import Snoowrap from "snoowrap";
 import "dotenv/config";
+import express from "express";
 
-const snoowrap = new Snoowrap({
-  userAgent: process.env.REDDIT_USER_AGENT,
-  clientId: process.env.REDDIT_CLIENT_ID,
-  clientSecret: process.env.REDDIT_CLIENT_SECRET,
-  username: process.env.REDDIT_USERNAME,
-  password: process.env.REDDIT_PASSWORD,
-});
+const app = express();
 
-snoowrap
-  .getSubreddit("AskReddit")
-  .getWikiPage("bestof")
-  .content_md.then(console.log);
+const PORT = process.env.PORT || 8080;
+
+// const snoowrap = new Snoowrap({
+//   userAgent: process.env.REDDIT_USER_AGENT,
+//   clientId: process.env.REDDIT_CLIENT_ID,
+//   clientSecret: process.env.REDDIT_CLIENT_SECRET,
+//   username: process.env.REDDIT_USERNAME,
+//   password: process.env.REDDIT_PASSWORD,
+// });
+
+app.listen(PORT, () =>
+  console.log(`server listening on port http://localhost:${PORT}`)
+);
+
+app.get("/", (req, res) => {});
+
+// snoowrap
+//   .getSubreddit("AskReddit")
+//   .getWikiPage("bestof")
+//   .content_md.then(console.log);
