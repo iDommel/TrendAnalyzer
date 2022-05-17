836344416483521a4c8665c8042b14204a84b803
Dommel Iona <iona.dommel-prioux@epitech.eu>
2022-05-17 10:26:05 +0200

    WIP: nodemon and docker-compose

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+  const [testData, setTestData] = useState();
+
+  const getData = async () => {
+    const myHeaders = new Headers({
+      Accept: "application/json",
+    });
+
+    const rawData = await fetch("http://localhost:5000/askreddit/bestof", {
+      headers: myHeaders,
+      method: "GET",
+    });
+    if (rawData.ok && rawData.status === 200) {
+      console.log(rawData);
+      setData(rawData.message);
+    }
+  };
+
+  const getTestData = async () => {
+    const myHeaders = new Headers({
+      "Access-Control-Allow-Origin": "*",
+    });
+
+    const rawData = await fetch("http://localhost:5000/test", {
+      headers: myHeaders,
+      method: "GET",
+    });
+    if (rawData.ok && rawData.status === 200) {
+      console.log(rawData);
+      setTestData(rawData);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +52,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={getData}>Get Data</button>
+      <p>{JSON.stringify(data)}</p>
+      <button onClick={getTestData}>Get test Data</button>
+      <p>{JSON.stringify(testData)}</p>
     </div>
   );
 }
