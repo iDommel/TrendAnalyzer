5b768e1c4f1a0e3fbd48c6ef6a31fb5b982ab73b
Dommel Iona <iona.dommel-prioux@epitech.eu>
2022-05-17 10:27:09 +0200

    WIP: nodemon and docker-compose

--- server/index.js
+++ server/index.js
@@ -1,26 +1,40 @@
 import Snoowrap from "snoowrap";
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
-// const snoowrap = new Snoowrap({
-//   userAgent: process.env.REDDIT_USER_AGENT,
-//   clientId: process.env.REDDIT_CLIENT_ID,
-//   clientSecret: process.env.REDDIT_CLIENT_SECRET,
-//   username: process.env.REDDIT_USERNAME,
-//   password: process.env.REDDIT_PASSWORD,
-// });
+const snoowrap = new Snoowrap({
+  userAgent: process.env.REDDIT_USER_AGENT,
+  clientId: process.env.REDDIT_CLIENT_ID,
+  clientSecret: process.env.REDDIT_CLIENT_SECRET,
+  username: process.env.REDDIT_USERNAME,
+  password: process.env.REDDIT_PASSWORD,
+});
 
 app.listen(PORT, () =>
   console.log(`server listening on port http://localhost:${PORT}`)
 );
 
-app.get("/", (req, res) => {});
+app.get("/askreddit/bestof", (req, res) => {
+  snoowrap
+    .getSubreddit("AskReddit")
+    .getWikiPage("bestof")
+    .content_md.then((thing) => {
+      console.log(thing);
+      return res.status(200).send({ message: thing });
+    });
+});
 
-// snoowrap
-//   .getSubreddit("AskReddit")
-//   .getWikiPage("bestof")
-//   .content_md.then(console.log);
+app.get("/test", (req, res) => {
+  res.send("test");
+});
