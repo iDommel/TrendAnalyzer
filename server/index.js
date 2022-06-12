0dd3713913278a11fc5de223c36108f819d3b063
Maux Camille <mcamille@localhost.localdomain>
2022-06-12 17:36:05 +0200

    ADDED: get Nb New Posts and Comments

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import Snoowrap from "snoowrap";
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import axios from 'axios';
 
 const app = express();
 app.use(express.json());
@@ -35,13 +36,46 @@ app.get("/askreddit/bestof", (req, res) => {
     });
 });
 
+const getNbNewPosts = async (subReddit, resBody) => {
+  const url = `https://api.pushshift.io/reddit/search/submission/?subreddit=${subReddit.display_name}&after=24h&size=0&metadata=true`;
+
+  try {
+    const res = await axios.get(url);
+    return {
+            ...resBody,
+            nbNewPosts: res.data.metadata.total_results
+          };
+  } catch (err) {
+    console.log(err);
+  }
+  return resBody
+}
+
+const getNbNewComments = async (subReddit, resBody) => {
+  const url = `https://api.pushshift.io/reddit/search/comment/?subreddit=${subReddit.display_name}&after=24h&size=0&metadata=true`;
+
+  try {
+    const res = await axios.get(url);
+    return {
+            ...resBody,
+            nbNewComments: res.data.metadata.total_results
+          };
+  } catch (err) {
+    console.log(err);
+  }
+  return resBody
+}
+
 app.get("/", async (req, res) => {
   const { query } = req;
   console.log(query);
 
-  const newPosts = await snoowrap
-    .getSubreddit(query.param)
-    .getNew({ options: { time: "month" } });
-  console.log(newPosts.length);
+  const subReddit = await snoowrap.searchSubreddits({ query: query.param, limit: 1 })[0];
+  console.log(subReddit.title);
+  console.log(`Subscribers: ${subReddit.subscribers}`);
+
+  const response = {nbNewPost: newPosts.data.metadata.total_results, nbNewComment: newComments.data.metadata.total_results};
+
+  console.log(response);
   return res.status(200).send(query);
 });
