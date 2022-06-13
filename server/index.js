0d170432463461e489de3e69facb500a4edb2a51
Dommel Iona <iona.dommel-prioux@epitech.eu>
2022-06-13 13:20:34 +0200

    ADDED: per subscriber stats

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import Snoowrap from "snoowrap";
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
-
+import axios from "axios";
 const app = express();
 app.use(express.json());
 app.use(
@@ -35,13 +35,147 @@ app.get("/askreddit/bestof", (req, res) => {
     });
 });
 
+const getCommentsPerUser = async (subReddit, resBody) => {
+  try {
+    const resp = await axios.get(
+      `https://api.pushshift.io/reddit/search/comment/?subreddit=${subReddit.display_name}&size=0&metadata=true`
+    );
+
+    const result = {
+      ...resBody,
+      commentsPerSub: resp.data.metadata.total_results / subReddit.subscribers,
+      totalComments: resp.data.metadata.total_results,
+    };
+    return result;
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+  return resBody;
+};
+
+const getPostsPerUser = async (subReddit, resBody) => {
+  const url = `https://api.pushshift.io/reddit/search/submission/?subreddit=${subReddit.display_name}&size=0&metadata=true`;
+  try {
+    const resp = await axios.get(url);
+
+    const result = {
+      ...resBody,
+      postsPerSub: resp.data.metadata.total_results / subReddit.subscribers,
+      totalPosts: resp.data.metadata.total_results,
+    };
+    return result;
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+  return resBody;
+};
+
+const getWordUsagePerSubreddit = async (keyword, resBody) => {
+  const url = `https://api.pushshift.io/reddit/search/comment/?q=${keyword}&after=7d&aggs=subreddit&size=0`;
+
+  try {
+    const resp = await axios.get(url);
+    console.log(resp.data);
+    console.log(resp.data.metadata);
+
+    const result = {
+      ...resBody,
+    };
+    return result;
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+  return resBody;
+};
+
+const getPostsWeekly = async (subReddit, resBody) => {
+  const url_last = `https://api.pushshift.io/reddit/search/submission/?subreddit=${subReddit.display_name}&after=7d&size=0&metadata=true`;
+  const url_snd_last = `https://api.pushshift.io/reddit/search/submission/?subreddit=${subReddit.display_name}&before=7d&after=14d&size=0&metadata=true`;
+
+  try {
+    const resp1 = await axios.get(url_last);
+    const resp2 = await axios.get(url_snd_last);
+
+    const totalPostsWeekly = resp1.data.metadata.total_results;
+    const totalPostsWeeklyPrev = resp2.data.metadata.total_results;
+    const increasePost =
+      ((totalPostsWeekly - totalPostsWeeklyPrev) / totalPostsWeeklyPrev) * 100;
+    const result = {
+      ...resBody,
+      postsPerSubWeekly:
+        resp1.data.metadata.total_results / subReddit.subscribers,
+      totalPostsWeekly,
+      postsPerSubWeeklyPrev:
+        resp2.data.metadata.total_results / subReddit.subscribers,
+      totalPostsWeeklyPrev,
+      increasePost,
+    };
+    return result;
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+  return resBody;
+};
+
+const getCommentsWeekly = async (subReddit, resBody) => {
+  const url_last = `https://api.pushshift.io/reddit/search/comment/?subreddit=${subReddit.display_name}&after=7d&size=0&metadata=true`;
+  const url_snd_last = `https://api.pushshift.io/reddit/search/comment/?subreddit=${subReddit.display_name}&before=7d&after=14d&size=0&metadata=true`;
+
+  try {
+    const resp1 = await axios.get(url_last);
+    const resp2 = await axios.get(url_snd_last);
+
+    const totalCommentsWeekly = resp1.data.metadata.total_results;
+    const totalCommentsWeeklyPrev = resp2.data.metadata.total_results;
+    const increaseComments =
+      ((totalCommentsWeekly - totalCommentsWeeklyPrev) /
+        totalCommentsWeeklyPrev) *
+      100;
+    const result = {
+      ...resBody,
+      postsPerSubWeekly:
+        resp1.data.metadata.total_results / subReddit.subscribers,
+      totalCommentsWeekly,
+      postsPerSubWeeklyPrev:
+        resp2.data.metadata.total_results / subReddit.subscribers,
+      totalCommentsWeeklyPrev,
+      increaseComments,
+    };
+    return result;
+  } catch (err) {
+    // Handle Error Here
+    console.error(err);
+  }
+  return resBody;
+};
+
 app.get("/", async (req, res) => {
   const { query } = req;
-  console.log(query);
+  let resBody = {};
+  if (!query.param)
+    return res.status(400).send({ message: "param is required" });
 
-  const newPosts = await snoowrap
-    .getSubreddit(query.param)
-    .getNew({ options: { time: "month" } });
-  console.log(newPosts.length);
-  return res.status(200).send(query);
+  // resBody = await getWordUsagePerSubreddit(query.param, resBody);
+  const subReddit = await snoowrap.searchSubreddits({
+    query: query.param,
+    limit: 1,
+  })[0];
+  if (subReddit) {
+    resBody = await getCommentsPerUser(subReddit, resBody);
+    resBody = await getPostsPerUser(subReddit, resBody);
+    resBody = await getPostsWeekly(subReddit, resBody);
+    resBody = await getCommentsWeekly(subReddit, resBody);
+    resBody = {
+      ...resBody,
+      subscribers: subReddit.subscribers,
+      discplay_name: subReddit.display_name,
+      title: subReddit.title,
+    };
+    return res.status(200).send(resBody);
+  }
+  res.status(404).send("found no subreddit for that keyword");
 });
