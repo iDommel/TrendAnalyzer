b74296bd890a8b835b40f925dacd299f13e520ee
Maux Camille <mcamille@localhost.localdomain>
2022-06-14 10:33:08 +0200

    ADDED: front

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,15 +1,94 @@
 import React from "react";
 import { useState } from "react";
-import { Input, Breadcrumb, Layout, Menu } from "antd";
+import { Input, Breadcrumb, Layout, Menu, Col, Row, Statistic, Divider } from "antd";
 
 const { Search } = Input;
 const { Header, Content, Footer } = Layout;
+
+const dataList = [];
+
+class DivList extends React.Component {
+  constructor(props) { super(props); }
+
+  displayForm() {
+    let divs = [];
+    dataList.forEach((data) => {
+      divs.push(
+        <div className="site-statistic-demo-card">
+          <Row gutter={16}>
+            <Col span={9}>
+              <Statistic title="Subreddit title" value={data.title} />
+            </Col>
+            <Col span={12}>
+              <Statistic title="Subcribers" value={data.subscribers} />
+            </Col>
+          </Row>
+          <Divider />
+          <Row gutter={16}>
+            <Col span={9}>
+              <Statistic title="Comments" value={data.totalComments} />
+            </Col>
+            <Col span={9}>
+              <Statistic title="Comments per Subcribers" value={data.commentsPerSub} precision={2} />
+            </Col>
+            <Col span={6}>
+              <Statistic title="New Comments (last 24h)" value={data.nbNewComments} precision={2} />
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={9}>
+              <Statistic title="Comments weekly" value={data.totalCommentsWeekly} />
+            </Col>
+            <Col span={9}>
+              <Statistic title="Comments last week" value={data.totalCommentsWeeklyPrev} precision={2} />
+            </Col>
+            <Col span={6}>
+              <Statistic title="Comments increase" value={data.increaseComments} precision={2} suffix="%"/>
+            </Col>
+          </Row>
+          <Divider />
+          <Row gutter={16}>
+            <Col span={9}>
+              <Statistic title="Posts" value={data.totalPosts} />
+            </Col>
+            <Col span={9}>
+              <Statistic title="Posts per Subcribers" value={data.postsPerSub} precision={2} />
+            </Col>
+            <Col span={6}>
+              <Statistic title="New Posts (last 24h)" value={data.nbNewPosts} precision={2} />
+            </Col>
+          </Row>
+          <Row gutter={16}>
+            <Col span={4}>
+              <Statistic title="Posts weekly" value={data.totalPostsWeekly} />
+            </Col>
+            <Col span={5}>
+              <Statistic title="Posts weekly per Subcribers" value={data.postsPerSubWeekly} precision={2} />
+            </Col>
+            <Col span={4}>
+              <Statistic title="Posts last week" value={data.totalPostsWeeklyPrev} precision={2} />
+            </Col>
+            <Col span={5}>
+              <Statistic title="Posts last week per Subcribers" value={data.postsPerSubWeeklyPrev} precision={2} />
+            </Col>
+            <Col span={4}>
+              <Statistic title="Posts increase" value={data.increasePost} precision={2} suffix="%"/>
+            </Col>
+          </Row>
+        </div>
+      );
+    });
+    return divs;
+  }
+
+  render() { return (<div> {this.displayForm()} </div>) }
+}
 
 export default function Home() {
   const [data, setData] = useState();
 
   const getData = async (keyword) => {
-    const resp = await fetch(`http://localhost:5000/?param=${keyword}`, {
+    const resp = await fetch(`http://localhost:8000/?param=${keyword}`, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -19,6 +98,8 @@ export default function Home() {
     if (resp.ok && resp.status === 200) {
       console.log(data);
       setData(data);
+      dataList.unshift(data);
+      console.log(dataList);
     }
   };
   return (
@@ -53,7 +134,8 @@ export default function Home() {
           size="large"
           onSearch={async (value) => getData(value)}
         />
-        <p>{JSON.stringify(data)}</p>
+
+        <DivList />
       </Content>
       <Footer
         style={{
